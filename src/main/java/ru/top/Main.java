ab00343a2f53dbaf2514c1ad2545ca31e4f0dbe6
Daniil <nektos13@yandex.ru>
2023-02-05 16:58:39 +0300

    Создал game и вызвал play Оставил имена игроков нулями

--- src/main/java/ru/top/Main.java
+++ src/main/java/ru/top/Main.java
@@ -1,7 +1,13 @@
 package ru.top;
 
+import ru.top.models.Game;
+import ru.top.models.Player;
+
 public class Main {
     public static void main(String[] args) {
-
+        Player firstPlayer = new Player(null);
+        Player secondPlayer = new Player(null);
+        Game game = new Game(firstPlayer,secondPlayer);
+        game.play();
     }
 }
