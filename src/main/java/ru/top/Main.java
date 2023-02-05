ce59c54afdf20097056bff04e652663ef708d29d
Daniil <nektos13@yandex.ru>
2023-02-05 17:26:41 +0300

    Создал game и вызвал play

--- src/main/java/ru/top/Main.java
+++ src/main/java/ru/top/Main.java
@@ -3,7 +3,7 @@ package ru.top;
 import ru.top.models.Game;
 import ru.top.models.Player;
 
-import java.io.BufferedReader;
+
 import java.util.Scanner;
 
 public class Main {
@@ -11,11 +11,11 @@ public class Main {
         System.out.println("Введите имя первого игрока...");
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
-        System.out.println("Введите имя второго игрока...");
-        Scanner scanner1 = new Scanner(System.in);
-        String name1 = scanner1.nextLine();
         Player firstPlayer = new Player(name);
-        Player secondPlayer = new Player(name1);
+        System.out.println("Введите имя второго игрока...");
+        scanner = new Scanner(System.in);
+        name = scanner.nextLine();
+        Player secondPlayer = new Player(name);
         Game game = new Game(firstPlayer, secondPlayer);
         game.play();
     }
